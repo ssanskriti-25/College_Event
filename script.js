95d6034f8300fd5e21520cd1b164c75dbab19e4d
Sanskriti Singh <ssanskriti647@gmail.com>
2024-06-04 01:48:16 +0530

    first commit

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ if(close){
         nav.classList.remove('active');
     })
 }
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Select the register button
+    const registerButton = document.getElementById('register');
+
+    // Add click event listener to the button
+    registerButton.addEventListener('click', function() {
+        const url = 'https://forms.gle/oZ1u9SE1vtKBteFUA'; // Replace with the actual registration URL
+        window.open(url, '_blank'); // Open the link in a new tab
+    });
+});
+
